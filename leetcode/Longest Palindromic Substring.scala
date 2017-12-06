9727a6c6ccb0e0a1b845c497ca06302a4ceecd9f
poem <qf.zgs.1994@gmail.com>
2017-12-06 21:54:15 +0800

    Update Longest Palindromic Substring.scala

--- leetcode/Longest Palindromic Substring.scala
+++ leetcode/Longest Palindromic Substring.scala
@@ -22,3 +22,28 @@ object Solution {
         "";
     }
 }
+
+# better solution 
+object Solution {
+    def longestPalindrome(s: String): String = {
+        val f = Array.ofDim[Boolean](1001,1001);
+        var len = s.length();
+        var maxlen = 0;
+        var l = 0; 
+        var r = 0;
+        for (i<-len-1 to 0 by -1; j<-i to len-1) {
+            f(i)(j) = i match {
+                case x if x==j => true ;
+                case x if x==j-1 => if (s(i) == s(j)) true else false;
+                case x if x<j-1 => if (s(i) == s(j)) f(i+1)(j-1) else false;
+            }
+            
+            if (f(i)(j)==true && maxlen < j - i + 1) {
+                maxlen = j - i + 1;
+                l = i;
+                r = j;
+            }  
+        }
+        s.substring(l,r+1);
+    }
+}
